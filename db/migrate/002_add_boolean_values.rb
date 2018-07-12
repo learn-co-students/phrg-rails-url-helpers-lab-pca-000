d1ff022f4b03dd81213020ec29c8a0c5dfaf212c
Daniel-Mack <daniel.mack@powerhrg.com>
2018-07-12 08:48:23 -0400

    finished lab

---
+++ db/migrate/002_add_boolean_values.rb
@@ -0,0 +1,5 @@
+class AddBooleanValues < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, :default => false
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+  student = Student.create(first_name: "Daniel", last_name: "Mack")
